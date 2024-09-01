60365bfbea3c02acfb616800ed04c66b5075271e
nmanyange <nmanyange30@gmail.com>
2024-09-01 10:16:26 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function updateWeather(response) {
     let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#app-icon"); 
     
-    iconElement.innerHTML=`<img src="${response.condition.icon.url}" class="app-icon"`;
+    iconElement.innerHTML=`<img src="${response.data.condition.icon.url}" class="app-icon" />`;
 
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
